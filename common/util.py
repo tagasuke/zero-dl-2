3ebcbc7317f4f904e632b62564879eef3a9d1caa
tagasuke <tawaga@gmail.com>
2018-08-11 14:57:46 +0000

    p74

--- common/util.py
+++ common/util.py
@@ -34,3 +34,7 @@ def create_co_matrix(corpus,vocab_size, window_size=1):
                 co_matrix[word_id, right_word_id] += 1
     return co_matrix
 
+def cos_similarity(x, y, eps=1e-8);
+    nx = x / (np.sqrt(np.sum(x ** 2)) + eps)
+    ny = y / (np.sqrt(np.sum(y ** 2)) + eps)
+    return np.dot(nx, ny)
